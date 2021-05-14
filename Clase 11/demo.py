bad280b52cee8a39f785024bc2ff2fe374e34ee9
smessina <silvio.messina@eant.tech>
2021-05-13 21:51:35 -0300

    Creando endpoint con un JSON de respuesta

--- Clase 11/demo.py
+++ Clase 11/demo.py
@@ -4,7 +4,7 @@ Created on Thu May 13 20:10:13 2021
 
 @author: EANT
 """
-from flask import Flask
+from flask import Flask, json
 
 app = Flask(__name__)
 
@@ -13,4 +13,24 @@ def hello_flask():
     
     return "Hola desde la home de Flask :D"
 
-app.run( port = 3000 )
+########################
+@app.route("/users")
+def usersTwitter():
+    users = [
+        { 'name' : 'smessina_' },
+        { 'name' : 'eanttech' },
+        { 'name' : 'TinchoLutter' },
+        { 'name' : 'bitcoinArg' }
+    ]
+    
+    response = app.response_class(response = json.dumps(users), status = 200, mimetype = "application/json")
+    
+    return response
+
+
+app.run( port = 3000, host = "0.0.0.0" )
+
+
+
+
+
