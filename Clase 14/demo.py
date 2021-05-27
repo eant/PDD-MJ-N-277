848b6cb67a6881ce87e40f3966590dcf2b74596b
smessina <silvio.messina@eant.tech>
2021-05-27 19:42:13 -0300

    Copia de archivos para Clase 14

---
+++ Clase 14/demo.py
@@ -0,0 +1,97 @@
+from flask import Flask, json
+from pymongo import MongoClient
+import settings
+from os import environ
+from urllib.parse import urlencode
+
+USER = environ['USER']
+PASS = environ['PASS']
+SERVER = environ['SERVER']
+DB = environ['DB']
+
+params = {
+   'retryWrites' : 'true',
+   'w' : 'majority',
+   'ssl' : 'true',
+   'ssl_cert_reqs' : 'CERT_NONE'
+}
+
+url = 'mongodb+srv://'+ USER + ':' + PASS +'@' + SERVER + '/' + DB + '?' + urlencode(params)
+
+print("Esta es la connection string:")
+print( url )
+
+client = MongoClient(url)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_flask():
+    
+    return "<h1>Hola desde la home de <u>Flask</u> :D</h1>"
+
+########################
+@app.route("/users")
+def usersTwitter():
+    users = [
+        { 'name' : 'smessina_' },
+        { 'name' : 'eanttech' },
+        { 'name' : 'TinchoLutter' },
+        { 'name' : 'bitcoinArg' }
+    ]
+    
+    response = app.response_class(response = json.dumps(users), status = 200, mimetype = "application/json")
+    
+    return response
+
+########################
+@app.route("/users/<path>")
+def searchUsers(path):
+    if path == "people":
+        return "Aca va un JSON de personas..."
+    elif path == "company":
+        return "Aca va un JSON de empresas..."
+    else:
+        return "Upps... no puedo mostrar lo que estás pidiendo :P"
+
+
+@app.route("/api/tweets/<usuario>")
+def getTweetsSinLimite(usuario):
+    return "OUT OF SERVICE"
+#########################
+@app.route("/api/tweets/<usuario>/<limite>")
+def getTweets(usuario, limite):
+    
+    bigdata = client['bigdata']
+    tweets = bigdata['tweets']
+    
+    
+    if limite != None and limite.isnumeric():
+        limite = int(limite)
+    else:
+        return "Sín resultados para la búsqueda"
+        
+    mis_tweets = tweets.find({'in_reply_to_screen_name': usuario}).limit(limite)
+    
+    res = []
+    
+    for tweet in mis_tweets:
+        
+        el_tweet = {
+            'id': tweet['id_str'],
+            'user': tweet['in_reply_to_screen_name'],
+            'txt': tweet['full_text']
+        }
+        res.append(el_tweet)
+        
+    response = app.response_class(response = json.dumps(res), status = 200, mimetype = "application/json")
+    
+    return response
+  
+
+
+app.run( port = 3000 , debug = True)
+
+
+
